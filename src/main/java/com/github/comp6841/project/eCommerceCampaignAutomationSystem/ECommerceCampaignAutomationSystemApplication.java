a44074ed6b12e1145d33e697c089a641b33f8b55
Prashanthi Ramesh <rvprashanthi@gmail.com>
2018-11-04 15:47:53 -0500

    - Initial Commit

---
+++ src/main/java/com/github/comp6841/project/eCommerceCampaignAutomationSystem/ECommerceCampaignAutomationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.github.comp6841.project.eCommerceCampaignAutomationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCampaignAutomationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCampaignAutomationSystemApplication.class, args);
+	}
+}
