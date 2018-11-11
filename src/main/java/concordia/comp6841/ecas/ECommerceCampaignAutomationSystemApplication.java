a88af7d328868366190804b1c9def9c5d99e6db7
Prashanthi Ramesh <rvprashanthi@gmail.com>
2018-11-11 17:14:24 -0500

    - security without db connection

--- src/main/java/concordia/comp6841/ecas/ECommerceCampaignAutomationSystemApplication.java
+++ src/main/java/concordia/comp6841/ecas/ECommerceCampaignAutomationSystemApplication.java
@@ -1,4 +1,4 @@
-package com.github.comp6841.project.eCommerceCampaignAutomationSystem;
+package concordia.comp6841.ecas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
