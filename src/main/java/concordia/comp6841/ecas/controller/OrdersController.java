b4e6802657442c6354bbe9222f0731c2dd5fe87d
Prashanthi Ramesh <rvprashanthi@gmail.com>
2018-11-24 19:15:00 -0500

    - products display done

--- src/main/java/concordia/comp6841/ecas/controller/OrdersController.java
+++ src/main/java/concordia/comp6841/ecas/controller/OrdersController.java
@@ -1,16 +1,25 @@
 package concordia.comp6841.ecas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import concordia.comp6841.ecas.repository.OrderRepository;
+import concordia.comp6841.ecas.repository.ProductRepository;
+
 @Controller
 @RequestMapping("/order")
 public class OrdersController {
 
+
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@GetMapping("/all")
-    public String showOrdersForm(Model model) {
-        return "order";
-    }
+	public String showOrdersForm(Model model) {
+		model.addAttribute("orders", orderRepository.findAll());
+		return "order";
+	}
 }
--- src/main/java/concordia/comp6841/ecas/controller/ProductsController.java
+++ src/main/java/concordia/comp6841/ecas/controller/ProductsController.java
@@ -1,16 +1,23 @@
 package concordia.comp6841.ecas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import concordia.comp6841.ecas.repository.ProductRepository;
 
 @Controller
 @RequestMapping("/product")
 public class ProductsController {
 
+
+	@Autowired
+	private ProductRepository productRepository;
+
 	@GetMapping("/all")
-    public String showProductsForm(Model model) {
-        return "product";
-    }
+	public String showProductsForm(Model model) {
+		model.addAttribute("products", productRepository.findAll());
+		return "product";
+	}
 }
