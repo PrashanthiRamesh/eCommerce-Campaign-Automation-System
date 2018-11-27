b72ef7b0a2da43035755d5c8099f9534b59b9130
Prashanthi Ramesh <rvprashanthi@gmail.com>
2018-11-27 03:03:57 -0500

    - customer groups done

--- src/main/java/concordia/comp6841/ecas/controller/ActiveCustomersController.java
+++ src/main/java/concordia/comp6841/ecas/controller/ActiveCustomersController.java
@@ -1,21 +1,24 @@
 package concordia.comp6841.ecas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import concordia.comp6841.ecas.repository.ActiveCustomerRepository;
 
 
 @Controller
 @RequestMapping("/active_customer")
 public class ActiveCustomersController {
 
-//	@Autowired
-//	private AbandonedCartRepository abandonedCartRepository;
+	@Autowired
+	private ActiveCustomerRepository activeCustomerRepository;
 
 	@GetMapping("/all")
 	public String showActiveCustomerForm(Model model) {
-		//model.addAttribute("abandoned_carts", abandonedCartRepository.findAll());
+		model.addAttribute("active_customers", activeCustomerRepository.findAll());
 		return "active_customer";
 	}
 }
--- src/main/java/concordia/comp6841/ecas/controller/InactiveCustomersController.java
+++ src/main/java/concordia/comp6841/ecas/controller/InactiveCustomersController.java
@@ -1,21 +1,24 @@
 package concordia.comp6841.ecas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import concordia.comp6841.ecas.repository.InactiveCustomerRepository;
 
 
 @Controller
 @RequestMapping("/inactive_customer")
 public class InactiveCustomersController {
 
-//	@Autowired
-//	private AbandonedCartRepository abandonedCartRepository;
+	@Autowired
+	private InactiveCustomerRepository inactiveCustomerRepository;
 
 	@GetMapping("/all")
 	public String showAbandonedCartForm(Model model) {
-	//	model.addAttribute("abandoned_carts", abandonedCartRepository.findAll());
+		model.addAttribute("inactive_customers", inactiveCustomerRepository.findAll());
 		return "inactive_customer";
 	}
 }
